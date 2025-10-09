7d515a499e2a861ea285cc8be7942db8a4264c46
uri3lrex <khanhe@tcd.ie>
2025-10-09 14:26:58 +0100

    doesn't work

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.use(express.json());
 const PORT = 3000;
 const apiKey = process.env.OPENWEATHER_API_KEY;
 
-// Simple test route to check weather for a given city
-app.get('/weather/:city', async (req, res) => {
-  const city = req.params.city;
-
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const response = await axios.get(url);
@@ -34,7 +30,18 @@ app.get('/weather/:city', async (req, res) => {
     console.error('Error fetching weather:', error.message);
     res.status(500).json({ error: 'Failed to fetch weather data' });
   }
-});
+
+// Simple test route to check weather for a given city
+//app.get('/weather/:city', async (req, res) => {
+ // const city = req.params.city;
+
+ app.get('/weather/:city', bringWeather)
+ app.listen(PORT,()=>console.log('Prototype listening on port ${port}!'))
+
+ function bringWeather(req,res){
+
+ }
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
