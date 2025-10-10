0763c6427b4302bde159c2633aee2a8e375ddd14
uri3lrex <khanhe@tcd.ie>
2025-10-10 09:27:26 +0100

    doesn't work help

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(express.json());
 
 const PORT = 3000; //pls work
 const apiKey = process.env.OPENWEATHER_API_KEY;
+const mapURL = 'https://api.openweathermap.org/data/2.5';
 
 app.get('/weather/:city', async (req, res) => {
   const city = req.params.city;
   try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `${mapURL}/weather?q=${city}&appid=${apiKey}&units=metric`;
     const response = await axios.get(url);
     const weather = response.data;
 
@@ -30,6 +31,58 @@ app.get('/weather/:city', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch weather data' });
   }
 });
+
+app.get('/forecast/:city', async (req,res)=> {
+  const city = req.params.city;
+  try {
+    const url = `${mapURL}/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await axios.get(url);
+    const forecastlist = response.data.list;
+
+    const forecast = [];
+    const today = new Date();
+    for (i=0; i<3;i++){
+      const day = new Date(today);
+      day.setDate(today.getDate()+i);
+      const dayWord= day.toISOString().split('T')[0]; 
+      const entries = forecastlist.filter(item => item.dt_txt.startsWith(dayWord))
+
+      const temp = (entries.reduce((sum,item) => sum + item.main.temp,0)/entries.length).toFixed(1);
+      const wind = (entries.reduce((sum,item) => sum + item.wind.speed,0)/entries.length).toFixed(1);
+      const rain = entries.reduce((sum,item) => sum + (item.rain?.['3h'] || 0), 0).toFixed(1);
+
+      forecast.push({
+                day: dayWord,
+                temperature: parseFloat(temp),
+                wind: parseFloat(wind),
+                rain: parseFloat(rain)
+            });   
+
+    }
+
+    // packing??
+
+    let packing ='';
+    const avgTemperature = forecast.reduce((sum,d)=> sum+d.temperature,0)/forecast.length();
+    if (avgTemperature < 8)
+      packing = 'Cold';
+    else if(avgTemperature >= 8 && avgTemperature <=24)
+      packing= 'Mild';
+    else
+      packing = 'Hot';
+
+    if(forecast.some(d => d.rain > 0)) packing += ', Bring an umbrella!';
+
+    res.json({
+      city: response.data.city.name,
+      packingAdvice: packing, forecast
+    });
+
+  } catch (error) {
+    console.error('Error fetching weather:', error.message);
+    res.status(500).json({ error: 'Failed to fetch weather data' });
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
